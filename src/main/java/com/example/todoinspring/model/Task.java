f84d5a6eb509a01ec1932e31b7bac21e86a65823
kabbo25 <kabbo25@gmail.com>
2024-04-15 15:56:41 +0600

    feat: title validation implemented

--- src/main/java/com/example/todoinspring/model/Task.java
+++ src/main/java/com/example/todoinspring/model/Task.java
@@ -1,57 +1,25 @@
 package com.example.todoinspring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="TODO")
+@Table(name = "TODO")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
     private String description;
     private boolean status;
 
-    // Constructors
-    public Task() {
-    }
 
-    public Task(String name, String description, boolean status) {
-        this.name = name;
-        this.description = description;
-        this.status = status;
-    }
-
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
 }
